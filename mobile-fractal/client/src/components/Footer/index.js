8ab518e692318908e94754027635fe41c91bf163
unknown <whalencollin@gmail.com>
2022-03-09 09:43:19 -0600

    canvas is functioning fairly well after draw params modified

---
+++ mobile-fractal/client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './style.css';
+
+const Footer = () => {
+    return (
+        <div>
+
+
+
+        </div>
+    )
+}
+
+export default Footer;
---
+++ mobile-fractal/client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <div>
+
+
+
+        </div>
+    )
+}
+
+export default Header;
